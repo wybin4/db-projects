cf9427ad12a25a3984d9df79d2aa18ff735efaae
ajdivotf <91722656+ajdivotf@users.noreply.github.com>
2022-09-21 21:47:26 +0300

    Update number_guess.sh

--- number guessing game/number_guess.sh
+++ number guessing game/number_guess.sh
@@ -1,49 +1,102 @@
 #!/bin/bash
 
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
-SECRET=$((1 + RANDOM % 1000))
-echo $SECRET
-COUNT_GUESSES=1
-echo -e "\nEnter your username:\n"
-read USERNAME
-FIND_IN_DB=$($PSQL "SELECT COUNT(game_id) FROM games WHERE username='$USERNAME'")
-BEST_GAME=$($PSQL "SELECT MIN(number_of_guesses) FROM games WHERE username='$USERNAME'")
-if [[ $FIND_IN_DB -ge $COUNT_GUESSES ]]
-then
-  echo "Welcome back, $USERNAME! You have played $FIND_IN_DB games, and your best game took $BEST_GAME guesses."
-else
-  echo -e "\nWelcome, $USERNAME! It looks like this is your first time here."
-fi
 
-MAIN_MENU() {
+STRING_LENGTH() {
+
+STRING="$1"
+echo "$STRING" | wc --chars | while read CHAR_COUNT
+do 
+  echo $(($CHAR_COUNT -1))
+done
+}
+
+WELCOME_MENU() {
+
   if [[ $1 ]]
   then
     echo -e "\n$1"
+  else
+    echo -e "\nEnter your username:"
   fi
-  GUESS_NUMBER
+
+  read USER_NAME
+
+  USER_NAME_LENGTH=$(STRING_LENGTH $USER_NAME)
+  if [[ $USER_NAME_LENGTH == 0 ]]
+  then
+    WELCOME_MENU -e "\nUsername should not be empty!"
+  elif [[ $USER_NAME_LENGTH > 30 ]]
+  then
+    WELCOME_MENU -e "\nUsername is too long!"
+  fi
+  USER_ID=$($PSQL "SELECT user_id FROM users WHERE user_name='$USER_NAME'")
+  if [[ $USER_ID ]]
+  then
+    
+    TOTAL_GAMES=$($PSQL "SELECT COUNT(*) FROM games WHERE user_id=$USER_ID")
+    BEST_GAME=$($PSQL"SELECT MIN(game_guesses) FROM games WHERE user_id=$USER_ID")
+    echo -e "\nWelcome back, $USER_NAME! You have played $TOTAL_GAMES games, and your best game took $BEST_GAME guesses."
+  else
+    
+    INSERT_USER_RESULT=$($PSQL "INSERT INTO users(user_name) VALUES('$USER_NAME')")
+    if [[ $INSERT_USER_RESULT = "INSERT 0 1" ]]
+    then 
+      echo -e "\nWelcome, $USER_NAME! It looks like this is your first time here."
+    else
+      echo -e "\nSomething went wrong while registering a new user!"
+    fi
+    USER_ID=$($PSQL "SELECT user_id FROM users WHERE user_name='$USER_NAME'")
+  fi
+
+  START_GAME
 }
-GUESS_NUMBER(){
-  echo "Guess the secret number between 1 and 1000:"
-  read TRY
-  if [[ $TRY -eq $SECRET ]]
-  then
-    echo "You guessed it in $COUNT_GUESSES tries. The secret number was $SECRET. Nice job!"
-  fi
-  if [[ ! $TRY =~ ^[0-9]+$ ]]
-  then
-    ((COUNT_GUESSES++))
-    MAIN_MENU "That is not an integer, guess again:"
-  fi
-  if [[ $TRY -gt $SECRET ]]
-  then
-    ((COUNT_GUESSES++))
-    MAIN_MENU "It's higher than that, guess again:"
-  fi
-  if [[ $TRY -lt $SECRET ]]
-  then
-    ((COUNT_GUESSES++))
-    MAIN_MENU "It's lower than that, guess again:"
-  fi
+
+START_GAME() {
+  RANDOM_NUMBER=$(( RANDOM % 1000 + 1 ))
+  GUESS_COUNT=0
+  GUESS
 }
-MAIN_MENU
-INSERT_VALUES=$($PSQL "INSERT INTO games(username, number_of_guesses) VALUES('$USERNAME', $COUNT_GUESSES)")
+
+GUESS() { 
+  if [[ $1 ]]
+  then
+    echo -e "\n$1"
+  else
+  echo -e "\nGuess the secret number between 1 and 1000:"
+  fi 
+
+  read USER_GUESS 
+
+  if [[ ! $USER_GUESS =~ ^[0-9]*$ ]]
+  then
+    GUESS "That is not an integer, guess again:"
+  fi
+
+  GUESS_COUNT=$(( GUESS_COUNT + 1 ))
+
+  if (( $USER_GUESS < $RANDOM_NUMBER ))
+  then
+    GUESS "It's higher than that, guess again:"
+  elif (( $USER_GUESS > $RANDOM_NUMBER ))
+  then
+    GUESS "It's lower than that, guess again:"
+  elif (( $USER_GUESS == $RANDOM_NUMBER ))
+  then
+    RECORD_GAME
+  fi
+  echo -e "\nSomething went wrong while guessing!"
+}
+
+RECORD_GAME() {
+    
+  INSERT_GAME_RESULT=$($PSQL "INSERT INTO games(game_guesses, user_id) VALUES($GUESS_COUNT, $USER_ID)")
+  if [[ $INSERT_GAME_RESULT == "INSERT 0 1" ]]
+  then
+    echo -e "\nYou guessed it in $GUESS_COUNT tries. The secret number was $RANDOM_NUMBER. Nice job!"
+    exit
+  fi
+  echo -e "\nSomething went wrong while registering a game record!"
+}
+
+WELCOME_MENU
